cd740768a541e3ea56a5e4d63f2d22a1afbf7698
Eric Perry <eric.s.perry@outlook.com>
2017-05-02 19:02:50 -0400

    C# Tutorial for Team TreehouseDefense

---
+++ TreehouseDefense/TreehouseDefense/Path.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Path
+    {
+        private readonly MapLocation[] _path;
+
+        public int Length => _path.Length;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            return (pathStep < _path.Length) ? _path[pathStep] : null;
+        }
+
+        public bool OnPath(Point point)
+        {
+            return point.X >= 0 && point.Y < _path.Length;
+        }
+    }
+}
---
+++ TreehouseDefense/TreehouseDefense/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Point
+    {
+        public readonly int X;
+        public readonly int Y;
+
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int DistanceTo(int x, int y)
+        {
+            int xDiff = X - x;
+            int yDiff = Y - y;
+
+            int xDiffSquared = xDiff * xDiff;
+            int yDiffSquared = yDiff * yDiff;
+
+            return (int)Math.Sqrt(xDiffSquared + yDiffSquared);
+        }
+
+        public int DistanceTo(Point point)
+        {
+            return DistanceTo(point.X, point.Y);
+        }
+    }
+}
